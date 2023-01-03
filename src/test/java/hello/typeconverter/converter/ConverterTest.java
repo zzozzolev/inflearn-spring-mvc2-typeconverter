24d8b2d01088f0ccc47265670a88bb778e56ea84
zzlev <gpalektzja@gmail.com>
2023-01-03 20:27:07 +0900

    Add converter tests.

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,22 @@
+package hello.typeconverter.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+}
